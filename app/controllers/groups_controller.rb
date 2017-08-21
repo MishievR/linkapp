af57353cffb92b4f025597e081d41cee6c2d44c4
Planetome Web App <roman@plametome.com>
2017-08-21 14:57:08 +0200

    logo and some ui improvments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,20 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:success] = "Group was updated succcessfully"
+      redirect_to group_path(@group)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @group = Group.find(params[:id])
     @person_groups = @group.people.paginate(page: params[:page], per_page: 4)
